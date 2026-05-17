d086c4f4d707b0f8c69299c471d89385abeb27f9
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-05-17 18:36:15 +0300

    Add Home, About, and Contact views with context data for featured and recent photos, and popular tags

--- core/views.py
+++ core/views.py
@@ -1,3 +1,48 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        try:
+            from gallery.models import Photo
+            context['featured_photos'] = Photo.objects.filter(
+                is_approved=True, 
+                is_featured=True
+            )[:12]
+            context['recent_photos'] = Photo.objects.filter(
+                is_approved=True
+            ).order_by('-created_at')[:12]
+        except:
+            context['featured_photos'] = []
+            context['recent_photos'] = []
+        
+        # Get popular tags
+        try:
+            from gallery.models import Tag
+            from django.db.models import Count
+            context['popular_tags'] = Tag.objects.annotate(
+                photo_count=Count('photos', filter=Count('photos__is_approved', True))
+            ).filter(photo_count__gt=0).order_by('-photo_count')[:10]
+        except:
+            context['popular_tags'] = []
+        
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['app_name'] = 'Photo Gallery'
+        context['version'] = '1.0.0'
+        return context
+
+
+class ContactView(TemplateView):
+    template_name = 'core/contact.html'
